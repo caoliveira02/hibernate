5e48bd299275333a6148b1c7951fdd3cf6dcd8f4
Carlos Alberto de Oliveira <ca.oliveira0265@gmail.com>
2021-04-05 16:28:17 -0300

    Inclusão das entidades Clientes e Pedidos

---
+++ loja/src/main/java/br/com/loja/produto/Pedido.java
@@ -0,0 +1,68 @@
+package br.com.loja.produto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private BigDecimal vlTotal;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Cliente cliente;
+		
+	public Pedido() {
+	}
+	
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getVlTotal() {
+		return vlTotal;
+	}
+
+	public void setVlTotal(BigDecimal vlTotal) {
+		this.vlTotal = vlTotal;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+		
+
+}
