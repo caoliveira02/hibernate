3ea6a781c559f74482f0423893a22b8bdedb4554
Carlos Alberto de Oliveira <ca.oliveira0265@gmail.com>
2021-04-05 13:01:46 -0300

    Inclusão da classe categoria

---
+++ loja/src/main/java/br/com/loja/dao/CategoriaDao.java
@@ -0,0 +1,19 @@
+package br.com.loja.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.loja.produto.Categoria;
+
+public class CategoriaDao {
+	
+	private EntityManager em;
+
+	public CategoriaDao(EntityManager em) {
+		super();
+		this.em = em;
+	}
+	
+	public void cadastrar(Categoria categoria) {
+		this.em.persist(categoria);
+	}
+}
